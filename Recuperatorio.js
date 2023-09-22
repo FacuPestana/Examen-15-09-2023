48ab83348adb2a9b8db5a6bd73f4cdf330b7ebd3
alumno <alumno@Instituto.local>
2023-09-22 09:30:09 -0300

    agregar archivos

---
+++ Recuperatorio.js
@@ -0,0 +1,17 @@
+//Punto 1 
+function IVA{
+x + 21%
+} 
+ 
+
+
+
+
+//Punto 2 
+let dolar = 740 
+prompt ('Ingresa la cantidad de dolares para pasar a pesos ')
+function Dolar(){
+    dolar = 740
+    prompt
+    return Dolar
+}
